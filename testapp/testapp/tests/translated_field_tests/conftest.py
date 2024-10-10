dda3b3221a089cd79f13c07a173b26ea6110ad7a
zaritskiyaa <zaritskiyaa@mail.ru>
2024-10-10 14:45:37 +0300

    add auto-translated test

--- testapp/testapp/tests/translated_field_tests/conftest.py
+++ testapp/testapp/tests/translated_field_tests/conftest.py
@@ -6,3 +6,8 @@ from .factories import QuestionFactory
 @pytest.fixture
 def obj_with_trans_field():
     return QuestionFactory.create()
+
+
+@pytest.fixture
+def model_obj():
+    return QuestionFactory.build()
--- testapp/testapp/tests/translated_field_tests/test_orm.py
+++ testapp/testapp/tests/translated_field_tests/test_orm.py
@@ -1,27 +1,25 @@
 import pytest
-from django.conf import settings
 
 from testapp.models import Question
 from .factories import QuestionFactory
-from extends import to_attribute
 
 
 @pytest.mark.django_db(transaction=True)
 class TestOverrideQuerySetInterface:
 
-    # TODO: надо перегружать константу LANGUAGES на тестовую.
     @pytest.mark.parametrize('attr, value', [('question', 'question'), ('answer', 'answer')])
     def test_create_method(self, attr, value):
 
         test_obj = QuestionFactory.create(**{attr: value})
+        desc = getattr(Question, attr)
 
         assert (
-            getattr(test_obj, to_attribute(attr)) == value
+            getattr(test_obj, desc.to_attribute(attr)) == value
         ), f'Object {test_obj} with data {attr, value} installed incorrectly.'
 
-        for code, _ in settings.LANGUAGES:
+        for code in desc.attr_suffix:
             assert hasattr(
-                test_obj, to_attribute(attr, code)
+                test_obj, desc.to_attribute(attr, code)
             ), f'Transalted field must set table attr for obj {test_obj}.'
 
     @pytest.mark.parametrize(
@@ -30,11 +28,38 @@ class TestOverrideQuerySetInterface:
     def test_update_method(self, obj_with_trans_field, attr, value):
 
         Question.objects.update(**{attr: value})
+        desc = getattr(Question, attr)
+
         try:
             obj_after_update = Question.objects.get(id=obj_with_trans_field.id)
         except Question.DoesNotExist:
             raise AssertionError('After query update object does not exists.')
 
-        assert getattr(obj_with_trans_field, to_attribute(attr)) != getattr(
-            obj_after_update, to_attribute(attr)
+        assert getattr(obj_with_trans_field, desc.to_attribute(attr)) != getattr(
+            obj_after_update, desc.to_attribute(attr)
         ), f'Object {obj_with_trans_field} attr {attr} not updated.'  # noqa E501
+
+    @pytest.mark.parametrize(
+            'method, attr, input_data, expect_result',
+            [
+                ('create', 'question', 'hello', ('hello', 'Здравствуйте', 'hallo')),
+                ('update', 'question', 'hello', ('hello', 'Здравствуйте', 'hallo')),
+            ],
+    )
+    def test_auto_translation(self, obj_with_trans_field, method, attr, input_data, expect_result):
+        desc = getattr(Question, attr)
+
+        if method == 'update':
+            id = obj_with_trans_field.id
+            Question.objects.update(**{attr: input_data})
+            q = Question.objects.get(id=id)
+        if method == 'create':
+            q = Question.objects.create(**{attr: input_data})
+
+            for expect, suf in zip(expect_result, desc.auto.suffix):
+                raw_field = desc.to_attribute(attr, suffix=suf)
+                result = getattr(q, raw_field, None)
+                assert result, f'Auto-translated work incorrect. Dont set required field {raw_field}' # noqa E501
+                assert result == expect, (
+                    f'Auto translated work incorrect expect {expect}, now {result}'
+                )
