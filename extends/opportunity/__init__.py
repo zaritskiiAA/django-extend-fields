3bfff1d4f55e9642fe5762df924531bb0f7687b3
zaritskiyaa <zaritskiyaa@mail.ru>
2024-10-11 15:26:00 +0300

    refactoring aws api handler methods for aws detector

--- extends/opportunity/__init__.py
+++ extends/opportunity/__init__.py
@@ -1,3 +1,10 @@
 from .bases import TranslatorBase, ApiHandler  # noqa
 from .translators import TextResultModel, DeeplTranslator, get_translator  # noqa
-from .language_detector import LangsModel, AWSLanguageDetector, get_detector  # noqa
+from .language_detector import (  # noqa
+    LangsModel,
+    BatchLangsModel,
+    AWSLanguageDetector,
+    get_detector,
+    AWSLangsModel,
+    AWSBatchLangsModel,
+)
